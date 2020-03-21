82d351c2cf3a393a80fe18862dd0355a2bc92eb1
Anna Kiniova <anna.kiniova@gmail.com>
2020-03-21 16:49:53 +0300

    feat: add node fetch function, await function

--- index.js
+++ index.js
@@ -1,36 +1,80 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (request, response)=> {
-    response.send(JSON.parse(userJSON));
-})
+// app.get('/', (request, response)=> {
+//     response.send(JSON.parse(userJSON));
+// })
+
+// app.post('/', (request, response)=> {
+
+//     response.send(userJSON);
+// })
+
+// app.delete('/', (request, response)=> {
+//     response.send('POST - Hello JS!');
+// })
+
+// app.get('/a', (request, response)=> {
+//     response.send('Hello JSa!');
+// })
+
+// // app.post('/', function (req, res) {
+// //     res.send('POST request to the homepage');
+// //   });
+
+// app.listen('8080');
 
 
-app.post('/', (request, response)=> {
+// let user = {
+//     name: "user Node",
+//     technology: "node.js",
+//     isAdmin: true
+// }
 
-    response.send(userJSON);
-})
+// let userJSON = JSON.stringify(user);
+// async function getData() {
+//     let response = await fetch('/article/fetch/logo-fetch.svg');
+//     let blob = await response.blob();
+//     let img = document.createElement('img');
+//     document.body.append(img);
+//     img.src = URL.createObjectURL(blob);
+    
+//     setTimeout(()=> {
+//     img.remove(); URL.revokeObjectURL(img.src);
+//     }, 3000);
 
-app.delete('/', (request, response)=> {
-    response.send('POST - Hello JS!');
-})
+// }
 
-app.get('/a', (request, response)=> {
-    response.send('Hello JSa!');
-})
+const fetch = require('node-fetch');
+async function getUserPr(names) {
+    let users = [];
 
-// app.post('/', function (req, res) {
-//     res.send('POST request to the homepage');
-//   });
+    for (let name of names) {
+        let response = fetch(`https://api.github.com/users/${name}`).then(
+            resolved => {
+                if (resolved.status != 200) {
+                    return null;
+                } else {
+                    return resolved.json();
+                }
+            },
+            rejected => {
+                return null;
+            }
+        );
+        users.push(response);
+        }
+    let results = await Promise.all(users);    
+    return(results);
+    }
+getUserPr(['afinika', 'sagero']).then(a=>console.log(a));
 
-app.listen('8080');
-
-
-let user = {
-    name: "user Node",
-    technology: "node.js",
-    isAdmin: true
-}
-
-let userJSON = JSON.stringify(user);
-//console.log(userJSON);
+// async function getUser(names) {
+//     for (let name of names) {
+//         let response = await fetch(`https://api.github.com/users/${name}`);
+//         if (response.status != 200) {
+//             return }
+//         let userName =  await response.json();
+//         console.log(userName);
+//     }
+// }
